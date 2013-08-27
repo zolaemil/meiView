e9581f4c99233b1f1f0a43dee4f20d129d0de850
Zoltan Komives <zolaemil@gmail.com>
2013-08-27 18:04:11 +0200

    Custom JQuery-UI theme
    Using JQuery-UI icons for pagination

--- js/meiview-ui.js
+++ js/meiview-ui.js
@@ -3,7 +3,7 @@ meiView.UI = {};
 meiView.UI.fillSideBar = function(sidebardiv, sources) {
   for(src in sources){
     var source = sources[src];
-    sidebardiv.append('<h3>'+src+'</h3><div><ul id="' + src + '"></ul></div>');
+    sidebardiv.append('<h3 class="meiView-sidebar">'+src+'</h3><div class="meiView-sidebar"><ul id="' + src + '"></ul></div>');
     var listElem = sidebardiv.find('ul[id="'+src+'"]');
     for (var i=0; i<source.length; i++) {
       listElem.append('<li>' + source[i].appID + '</li>')
