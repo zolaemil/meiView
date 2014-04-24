8d93139f38ad47bdce3479b750ff424a8b1a05e1
Zoltan Komives <zolaemil@gmail.com>
2014-04-24 18:43:44 +0100

    updated test cases

---
+++ test/test-util.js
@@ -0,0 +1,60 @@
+/*
+ * test-util.js 
+ *
+ * Helper functions for tests
+ *
+ */
+
+meiView.Test = (function () {
+  
+  test = {
+    
+    loadMEIDoc: function(filename, options) {
+      var loadedXML = loadXMLDoc(filename);
+      var filteredXml = meiView.filterMei(loadedXML, options);
+      var meiDoc = new MeiLib.MeiDoc(filteredXml);
+      return meiDoc;
+    },
+
+    LayedoutTest: function(tc) {
+      var meiDoc = this.loadMEIDoc(tc.filename);
+      var testdiv = $('<div/>', {
+        class: "test-div",
+        text: tc.title + ' - Layout View',
+      });
+      testdiv.appendTo(tc.div);
+      testdiv.append('<div class="viewer"></div>');
+      viewer = new meiView.Viewer({
+        maindiv: $(testdiv).find('.viewer'),
+        MEI: meiDoc,
+        pages: tc.pagination,
+        title: tc.title,
+      });
+      viewer.nextPage();
+      return viewer;
+    },
+
+    CompactTest: function(tc) {
+      var meiDoc = this.loadMEIDoc(tc.filename, { noSysBreak:true });
+      var testdiv = $('<div/>', {
+        class: "test-div",
+        text: tc.title + ' - Layout View',
+      });
+      testdiv.appendTo(tc.div);
+      testdiv.append('<div class="viewer"></div>');
+      viewer = new meiView.CompactViewer({
+        maindiv: $(testdiv).find('.viewer'),
+        MEI: meiDoc,
+        pages: tc.pagination,
+        title: tc.title,
+        displayFirstPage: true,
+      });
+      return viewer;
+    },
+
+  }
+
+  return test;
+  
+})();
+
